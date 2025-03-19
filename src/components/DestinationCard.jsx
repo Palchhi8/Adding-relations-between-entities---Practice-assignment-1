15d6310888704cb0afa5bb1778d57226a170f9ea
Palchhi8 <jainpalchhi2k6@gmail.com>
2025-03-19 09:55:26 +0530

    Done practice assignment - 1

---
+++ src/components/DestinationCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./DestinationCard.css"; // For styling
+
+const DestinationCard = ({ destination }) => {
+  return (
+    <div className="card">
+      <img src={destination.image} alt={destination.name} className="card-img" />
+      <div className="card-content">
+        <h2>{destination.name}</h2>
+        <h4>{destination.location}</h4>
+        <p>{destination.description}</p>
+        <p className="price">{destination.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DestinationCard;
